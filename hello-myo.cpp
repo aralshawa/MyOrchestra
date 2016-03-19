6b89c5388ceacbc82c5e2e56f233d8348e439372
Developer <Developer>
2016-03-19 18:03:28 -0400

    Added calibration.

--- hello-myo.cpp
+++ hello-myo.cpp
@@ -9,7 +9,7 @@
 #include <stdexcept>
 #include <string>
 #include <algorithm>
-
+#include <time.h>
 
 #include <windows.h>
 #include <mmsystem.h>
@@ -23,42 +23,43 @@
 // provides several virtual functions for handling different kinds of events. If you do not override an event, the
 // default behavior is to do nothing.
 class DataCollector : public myo::DeviceListener {
-	
-public:
-    DataCollector()
-    : onArmL(false), isUnlockedL(false), roll_w_l(0), pitch_w_l(0), yaw_w_l(0), past_pitch_l(0),
-	  onArmR(false), isUnlockedR(false), roll_w_r(0), pitch_w_r(0), yaw_w_r(0), past_pitch_r(0){
 
-    }
+public:
+	DataCollector()
+		: onArmL(false), isUnlockedL(false), roll_w_l(0), pitch_w_l(0), yaw_w_l(0), past_pitch_l(0),
+		onArmR(false), isUnlockedR(false), roll_w_r(0), pitch_w_r(0), yaw_w_r(0), past_pitch_r(0) {
+
+	}
 
 	void onPair(myo::Myo* myo, uint64_t timestamp, myo::FirmwareVersion firmwareVersion) {
 		knownMyos.push_back(myo);
 	}
 
-    // onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
-    void onUnpair(myo::Myo* myo, uint64_t timestamp) {
+	// onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
+	void onUnpair(myo::Myo* myo, uint64_t timestamp) {
 		if (identifyMyo(myo) == 1) {
 			onArmR = false;
 			isUnlockedR = false;
 			roll_w_r = 0;
 			pitch_w_r = 0;
 			yaw_w_r = 0;
-		} else if (identifyMyo(myo) == 2) {
+		}
+		else if (identifyMyo(myo) == 2) {
 			onArmL = false;
 			isUnlockedL = false;
 			roll_w_l = 0;
 			pitch_w_l = 0;
 			yaw_w_l = 0;
 		}
-    }
+	}
 
-    void onOrientationData(myo::Myo* myo, uint64_t timestamp, const myo::Quaternion<float>& quat) {
+	void onOrientationData(myo::Myo* myo, uint64_t timestamp, const myo::Quaternion<float>& quat) {
 
-        using std::atan2;
-        using std::asin;
-        using std::sqrt;
-        using std::max;
-        using std::min;
+		using std::atan2;
+		using std::asin;
+		using std::sqrt;
+		using std::max;
+		using std::min;
 		if (identifyMyo(myo) == 1) {
 			past_pitch_r = pitch_w_r;
 
@@ -76,7 +77,8 @@ public:
 
 			pitch_delta_r = pitch_w_r - past_pitch_r;
 
-		} else if (identifyMyo(myo) == 2) {
+		}
+		else if (identifyMyo(myo) == 2) {
 			past_pitch_l = pitch_w_l;
 
 			// Calculate Euler angles (roll, pitch, and yaw) from the unit quaternion.
@@ -93,43 +95,44 @@ public:
 
 			pitch_delta_l = pitch_w_l - past_pitch_l;
 		}
-    }
+	}
 
-    void onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm, myo::XDirection xDirection, float rotation,
-                   myo::WarmupState warmupState) {
+	void onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm, myo::XDirection xDirection, float rotation,
+		myo::WarmupState warmupState) {
 		if (identifyMyo(myo) == 1) {
 			onArmR = true;
-		} else if (identifyMyo(myo) == 2) {
+		}
+		else if (identifyMyo(myo) == 2) {
 			onArmL = true;
 		}
-    }
+	}
 
-    void onArmUnsync(myo::Myo* myo, uint64_t timestamp) {
+	void onArmUnsync(myo::Myo* myo, uint64_t timestamp) {
 		if (identifyMyo(myo) == 1) {
 			onArmR = false;
 		}
 		else if (identifyMyo(myo) == 2) {
 			onArmL = false;
 		}
-    }
+	}
 
-    void onUnlock(myo::Myo* myo, uint64_t timestamp) {
+	void onUnlock(myo::Myo* myo, uint64_t timestamp) {
 		if (identifyMyo(myo) == 1) {
 			isUnlockedR = true;
 		}
 		else if (identifyMyo(myo) == 2) {
 			isUnlockedL = true;
 		}
-    }
+	}
 
-    void onLock(myo::Myo* myo, uint64_t timestamp) {
+	void onLock(myo::Myo* myo, uint64_t timestamp) {
 		if (identifyMyo(myo) == 1) {
 			isUnlockedR = false;
 		}
 		else if (identifyMyo(myo) == 2) {
 			isUnlockedL = false;
 		}
-    }
+	}
 
 	size_t identifyMyo(myo::Myo* myo) {
 		// Walk through the list of Myo devices that we've seen pairing events for.
@@ -143,7 +146,19 @@ public:
 		return 0;
 	}
 
-    bool onArmL, onArmR;
+	void calibrate() {
+
+		lim_yaw_l[2] = ((0.3333)*(double)(lim_yaw_l[1] - lim_yaw_l[0])) + lim_yaw_l[0];
+		lim_yaw_l[3] = ((0.6667)*(double)(lim_yaw_l[1] - lim_yaw_l[0])) + lim_yaw_l[0];
+
+		lim_yaw_r[2] = ((0.3333)*(double)(lim_yaw_r[1] - lim_yaw_r[0])) + lim_yaw_r[0];
+		lim_yaw_r[3] = ((0.6667)*(double)(lim_yaw_r[1] - lim_yaw_r[0])) + lim_yaw_r[0];
+
+		lim_pitch_r[2] = ((0.6667)*(double)(lim_pitch_r[1] - lim_pitch_r[0])) + lim_pitch_r[0];
+
+	}
+
+	bool onArmL, onArmR;
 
 	bool isUnlockedL, isUnlockedR;
 
@@ -153,62 +168,68 @@ public:
 	int pitch_delta_l, pitch_delta_r;
 	int past_pitch_l, past_pitch_r;
 
-	static const int refresh_time = 50;
+	int lim_pitch_l[3] = { 0, 0, 0 };
+	int lim_yaw_l[4] = { 0, 0, 0, 0 };
+
+	int lim_pitch_r[3] = { 0, 0, 0 };
+	int lim_yaw_r[4] = { 0, 0, 0, 0 };
 
 	std::vector<myo::Myo*> knownMyos;
 };
 
 int main(int argc, char** argv) {
-    // We catch any exceptions that might occur below -- see the catch statement for more details.
-    try {
+	// We catch any exceptions that might occur below -- see the catch statement for more details.
+	try {
 
-    // First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
-    // publishing your application. The Hub provides access to one or more Myos.
-    myo::Hub hub("com.example.hello-myo");
+		// First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
+		// publishing your application. The Hub provides access to one or more Myos.
+		myo::Hub hub("com.example.hello-myo");
 
-    // Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
-    DataCollector collector;
+		// Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
+		DataCollector collector;
 
-    // Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
-    // Hub::run() to send events to all registered device listeners.
-    hub.addListener(&collector);
+		// Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
+		// Hub::run() to send events to all registered device listeners.
+		hub.addListener(&collector);
 
-    // Finally we enter our main loop.
-    while (1) {
-        // In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
-        // In this case, we wish to update our display 20 times a second, so we run for 1000/20 milliseconds.
-        hub.run(collector.refresh_time);
-        
+		// Determine range
+		hub.run(1000 / 20);
+		collector.lim_pitch_r[0] = collector.pitch_w_r; collector.lim_pitch_r[1] = collector.pitch_w_r;
+		collector.lim_yaw_r[0] = collector.yaw_w_r;   collector.lim_yaw_r[1] = collector.yaw_w_r;
 
+		collector.lim_pitch_l[0] = collector.pitch_w_l; collector.lim_pitch_l[1] = collector.pitch_w_l;
+		collector.lim_yaw_l[0] = collector.yaw_w_l;   collector.lim_yaw_l[1] = collector.yaw_w_l;
 
-		std::cout << "                              ";
-		std::cout << '\r';
-		std::cout << "Yaw: " << "[" << collector.yaw_w_l << "," << collector.yaw_w_r << "]";
+		for (int i = 0; i < 200; i++) {
+			hub.run(1000 / 20);
 
-		//if (collector.yaw_w_r > 300 || collector.yaw_w_r < 80) {
-		//	std::cout << "Section: " << "String     ";
-		//	if (collector.pitch_w_r > 220 || collector.pitch_w_r < 150) {
-		//		PlaySound("string.wav", NULL, SND_FILENAME);
-		//	}
-		//}
-		//else if (collector.yaw_w_r > 150) {
-		//	std::cout << "Section: " << "Percussion ";
-		//	if (collector.pitch_w_r > 220 || collector.pitch_w_r < 150) {
-		//		PlaySound("percussion.wav", NULL, SND_FILENAME);
-		//	}
-		//} else {
-		//	std::cout << "Section: " << "Woodwind   ";
-		//	if (collector.pitch_w_r > 220 || collector.pitch_w_r < 150) {
-		//		PlaySound("woodwind.wav", NULL, SND_FILENAME);
-		//	}
-		//}
-    }
+			if (collector.pitch_w_r < collector.lim_pitch_r[0]) { collector.lim_pitch_r[0] = collector.pitch_w_r; }
+			if (collector.pitch_w_r > collector.lim_pitch_r[1]) { collector.lim_pitch_r[1] = collector.pitch_w_r; }
+			if (collector.yaw_w_r   < collector.lim_yaw_r[0]) { collector.lim_yaw_r[0] = collector.yaw_w_r; }
+			if (collector.yaw_w_r   > collector.lim_yaw_r[1]) { collector.lim_yaw_r[1] = collector.yaw_w_r; }
 
-    // If a standard exception occurred, we print out its message and exit.
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        std::cerr << "Press enter to continue.";
-        std::cin.ignore();
-        return 1;
-    }
+			if (collector.pitch_w_l < collector.lim_pitch_l[0]) { collector.lim_pitch_l[0] = collector.pitch_w_l; }
+			if (collector.pitch_w_l > collector.lim_pitch_l[1]) { collector.lim_pitch_l[1] = collector.pitch_w_l; }
+			if (collector.yaw_w_l   < collector.lim_yaw_l[0]) { collector.lim_yaw_l[0] = collector.yaw_w_l; }
+			if (collector.yaw_w_l   > collector.lim_yaw_l[1]) { collector.lim_yaw_l[1] = collector.yaw_w_l; }
+		}
+
+		collector.calibrate();
+
+		// Finally we enter our main loop.
+		while (1) {
+			// In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
+			// In this case, we wish to update our display 20 times a second, so we run for 1000/20 milliseconds.
+			hub.run(1000 / 20);
+
+		}
+
+		// If a standard exception occurred, we print out its message and exit.
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		std::cerr << "Press enter to continue.";
+		std::cin.ignore();
+		return 1;
+	}
 }
